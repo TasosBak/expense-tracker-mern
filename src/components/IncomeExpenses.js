e7828c87649731b9f32da989a5e1f39ad8120e47
Anastasios Vakirtzis <anastasiosvakirtzis@gmail.com>
2020-06-17 18:16:15 +0300

    Added functionality to IncomeExpenses.js

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((item) => item.amount);
+  const incomeAmount = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expenseAmount = amounts
+    .filter((amount) => amount < 0)
+    .reduce((acc, item) => (acc -= item), 0)
+    .toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">{incomeAmount}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">{expenseAmount}</p>
       </div>
     </div>
   );
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -10,10 +10,6 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
         {transactions &&
           transactions.map((transaction) => (
             <TransactionItem transaction={transaction} key={transaction.id} />
